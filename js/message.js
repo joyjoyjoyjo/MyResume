b0bdf43834b8b65aede89aadee45ac533c4868b4
xiaoyiyi <xiaoyishikongde@163.com>
2019-01-21 00:55:28 +0800

    输入值不能为空

--- js/message.js
+++ js/message.js
@@ -1,53 +1,75 @@
-var APP_ID = 'v5w27JD80wqIR8qAsSqzyeMr-gzGzoHsz';
-var APP_KEY = 'Oy97U2VQmj7MUfpdvT7PJ4Pl';
+!function () {
+    var view = document.querySelector('section.message')
 
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
+    var model = {
+        init: function () {
+            var APP_ID = 'v5w27JD80wqIR8qAsSqzyeMr-gzGzoHsz'
+            var APP_KEY = 'Oy97U2VQmj7MUfpdvT7PJ4Pl'
+            AV.init({ appId: APP_ID, appKey: APP_KEY })
+        },
+        fetch: function () {
+            var query = new AV.Query('Message');
+            return query.find()
+        },
+        save: function (name, content) {
+            var Message = AV.Object.extend('Message')
+            var message = new Message()
+            return message.save({
+                content: content,
+                name: name
+            })
+        }
+    }
+    var controller = {
+        view: null,
+        model: null,
+        messageList: null,
+        init: function (view, model) {
+            this.view = view
+            this.model = model
+            this.messageList = view.querySelector(`#messageList`)
+            this.form = view.querySelector('form')
+            this.model.init()
+            this.loadMessages()
+            this.bindEvents()
+        },
+        loadMessages: function () {
+            this.model.fetch().then(
+                (messages) => {
+                    let array = messages.map((item) => item.attributes)
+                    array.forEach((item) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${item.name}:${item.content}`
+                        this.messageList.append(li)
+                    })
+                })
+        },
+        bindEvents: function () {
+            this.form.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.saveMessages()
+            })
+        },
+        saveMessages: function () {
+            let myform = this.form
+            let content = myform.querySelector('input[name=content]').value
+            let name = myform.querySelector('input[name=name]').value
+            if (content ==='' ||name==''||content ===' ' ||name==' '||content == null||name == null||name == undefined||content == undefined) {
+                alert("用户名或输入内容不能为空！")
+                myform.querySelector('input[name=content]').value = ''
+                myform.querySelector('input[name=name]').value = ''
+            } else {
+                this.model.save(name, content).then(function (object) {
+                    let li = document.createElement('li')
+                    li.innerText = `${object.attributes.name}:${object.attributes.content}`
+                    let messageList = document.querySelector('#messageList')
+                    messageList.append(li)
+                    myform.querySelector('input[name=content]').value = ''
+                })
+            }
+        }
+    }
+    controller.init(view, model)
+}.call()
 
-var query = new AV.Query('Message');
-query.find()
-    .then(function (messages) {
-        let array = messages.map((item) => item.attributes)
-        array.forEach((item) => {
-            let li = document.createElement('li')
-            li.innerText = `${item.name}:${item.content}`
-            let messageList = document.querySelector('#messageList')
-            messageList.append(li)
-        });
-    }, function (error) {
-        // 异常处理
-    })
 
-let myform = document.querySelector('#postMessageForm')
-
-myform.addEventListener('submit', function (e) {
-    e.preventDefault()
-    let content = myform.querySelector('input[name=content]').value
-    let name = myform.querySelector('input[name=name]').value
-    //创建一个叫Message的表
-    var Message = AV.Object.extend('Message')
-    //在表中创建一行数据
-    var message = new Message()
-    message.save({
-        content: content,
-        name: name
-    }).then(function (object) {
-        let li = document.createElement('li')
-        li.innerText = `${object.attributes.name}:${object.attributes.content}`
-        let messageList = document.querySelector('#messageList')
-        messageList.append(li)
-        myform.querySelector('input[name=content]').value = ''
-    })
-})
-
-//
-//var Message = AV.Object.extend('Message');
-
-///var testObject = new TestObject();
-//testObject.save({
- //   words: 'Hello World!'
-//}).then(function (object) {
-//    alert('LeanCloud Rocks!');
-//})
